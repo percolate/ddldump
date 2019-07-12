6e6aee54e2dd4e16943de912b80b0c7fc19c4996
Laurent Raufaste <analogue@glop.org>
2019-07-11 23:32:08 -0700

    More fixes

--- ddldump_postgres.sql
+++ ddldump_postgres.sql
@@ -5,6 +5,7 @@ CREATE TABLE "public"."alembic_version" (
 ALTER TABLE ONLY "public"."alembic_version" ADD CONSTRAINT "version_num_pkey" PRIMARY KEY ("version_num");
 COMMENT ON COLUMN "public"."alembic_version"."version_num" IS 'Migration Version number';
 COMMENT ON TABLE "public"."alembic_version" IS 'Track database migration versions.';
+SELECT pg_catalog.set_config('search_path', '', false);
 
 -- Create syntax for TABLE 'ddldump_test'
 CREATE TABLE "public"."ddldump_test" (
@@ -19,7 +20,7 @@ CREATE TABLE "public"."ddldump_test" (
     "deleted_at" timestamp without time zone
 );
 ALTER TABLE ONLY "public"."ddldump_test" ADD CONSTRAINT "ddldump_test_pkey" PRIMARY KEY ("id");
-ALTER TABLE ONLY "public"."ddldump_test" ADD CONSTRAINT "ddldump_test_parent_id_fkey" FOREIGN KEY ("parent_id") REFERENCES "ddldump_test"("id") ON DELETE CASCADE;
+ALTER TABLE ONLY "public"."ddldump_test" ADD CONSTRAINT "ddldump_test_parent_id_fkey" FOREIGN KEY ("parent_id") REFERENCES "public"."ddldump_test"("id") ON DELETE CASCADE;
 COMMENT ON COLUMN "public"."ddldump_test"."id" IS 'Percoflake ID of the saved query folder.';
 COMMENT ON COLUMN "public"."ddldump_test"."name" IS 'Name of the saved query folder.';
 COMMENT ON COLUMN "public"."ddldump_test"."type" IS 'Type of the saved query folder as defined in apps.data.saved_query.constants.TYPES';
