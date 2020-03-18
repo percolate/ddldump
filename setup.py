233538be9621b86d4d922368a3819677b4f2ec4a
Dimitry <dimitry.lukashov@percolate.com>
2020-03-18 17:32:47 -0400

    ran black; added postgresql-client back in

--- setup.py
+++ setup.py
@@ -4,32 +4,30 @@ from ddldump.constants import VERSION
 
 
 setup(
-    name='ddldump',
+    name="ddldump",
     version=VERSION,
-    author='Laurent Raufaste',
-    author_email='analogue@glop.org',
-    url='https://github.com/percolate/ddldump',
-    description=('Dump a clean version of the DDLs of your tables, so you can '
-                 'version them.'),
-    keywords=('ddldump ddl dump sql mysql postgresql sqlalchemy mysqldump '
-              'pg_dump'),
-    license='GPLv3',
-    packages=['ddldump'],
-    install_requires=['docopt', 'sqlalchemy'],
-    entry_points={
-        'console_scripts': [
-            'ddldump=ddldump.main:main',
-        ],
-    },
+    author="Laurent Raufaste",
+    author_email="analogue@glop.org",
+    url="https://github.com/percolate/ddldump",
+    description=(
+        "Dump a clean version of the DDLs of your tables, so you can " "version them."
+    ),
+    keywords=("ddldump ddl dump sql mysql postgresql sqlalchemy mysqldump " "pg_dump"),
+    license="GPLv3",
+    packages=["ddldump"],
+    install_requires=["docopt", "sqlalchemy"],
+    entry_points={"console_scripts": ["ddldump=ddldump.main:main"]},
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: End Users/Desktop',
-        ('License :: OSI Approved :: '
-         'GNU General Public License v3 or later (GPLv3+)'),
-        'Natural Language :: English',
-        'Operating System :: POSIX',
-        'Programming Language :: Python',
-        'Topic :: Utilities',
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        (
+            "License :: OSI Approved :: "
+            "GNU General Public License v3 or later (GPLv3+)"
+        ),
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
     ],
 )
